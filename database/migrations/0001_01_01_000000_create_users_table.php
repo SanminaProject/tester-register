923fc48216f06d03515432ab17916f54f96596b5
Helmi Griffiths <t3grhe00@students.oamk.fi>
2026-04-04 11:43:28 +0300

    refactor: delete repeating migrations file + update user migration file to include both first_name and last_name

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name', 100);
+            $table->string('first_name', 100)->nullable();
+            $table->string('last_name', 100)->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
 
