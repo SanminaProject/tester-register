c0f9815e744e72e357294f521abd781379d97be7
PrincessBubblegum007 <chli24@students.oamk.fi>
2026-04-04 21:57:17 +0300

    feat: Add sidebar component for Tester navigation

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,19 @@
+<div class="w-64 min-h-screen p-0 bg-white">
+  <h2 class="text-xl font-bold px-2 py-4" style="background-color: #E18BA1; color: #111;">
+    Testers
+  </h2>
+        <ul>
+        <li class="mb-2">
+            <a href="#" class="block p-2 rounded bg-red-200 text-red-900 font-semibold">All Testers</a>
+        </li>
+        <li class="mb-2">
+            <a href="#" class="block p-2 rounded hover:bg-red-50">Add New Tester</a>
+        </li>
+        <li class="mb-2">
+            <a href="#" class="block p-2 rounded hover:bg-red-50">Audit Logs</a>
+        </li>
+        <li>
+            <a href="#" class="block p-2 rounded hover:bg-red-50">Add New Log</a>
+        </li>
+    </ul>
+</div>
--- resources/views/tester.blade.php
+++ resources/views/tester.blade.php
@@ -1,15 +1,11 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Tester') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <div class="flex">
+        <x-sidebar />
+        <div class="flex-1 p-8">
+            {{--  Tester List Card component --}}
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    This is the tester page.
+                    This is the tester List component.
                 </div>
             </div>
         </div>
