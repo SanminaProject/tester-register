907d18ac6523c6e1a286d770dce3062e18488bf9
Jane Huang <ZhiyingHuang2023@outlook.com>
2026-04-11 15:57:51 +0300

    fix: navbar navigation and fixtures sidebar tabs/links navigation

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -4,7 +4,7 @@
     'activeTab' => null, 
 ])
 
-<div class="w-1/6 min-h-screen p-0 bg-white">
+<div class="w-1/5 min-h-screen p-0 bg-white">
     <h2 class="text-xl font-bold px-2 py-4 flex flex-col items-center bg-secondary text-black">
         {{ $title }}
     </h2>
@@ -15,15 +15,26 @@
         <ul class="flex flex-col">
             @foreach ($items as $item)
             @php
-                $isActive = isset($item['tab']) && $activeTab === $item['tab'];
+                $isActive = (isset($item['tab']) && $activeTab === $item['tab']) || 
+                            (isset($item['href']) && request()->fullUrlIs(url($item['href'])));
             @endphp
             
             <li class="{{ $isActive ? 'bg-thirdly font-bold' : 'border-b border-gray-100' }}">
-                <button
-                    wire:click="$set('activeTab', '{{ $item['tab'] }}')"
-                    class="block w-full px-4 py-4 font-normal text-base-medium text-canter text-black bg-transparent border-none outline-none cursor-pointer">
-                    {{ $item['label'] }}
-                </button>
+                
+                @if(isset($item['href']))
+                    <a href="{{ $item['href'] }}" 
+                       class="block w-full px-4 py-3 font-normal text-base text-center text-black bg-transparent no-underline">
+                        {{ $item['label'] }}
+                    </a>
+                    
+                @else
+                    <button
+                        wire:click="$set('activeTab', '{{ $item['tab'] }}')"
+                        class="block w-full px-4 py-3 font-normal text-base text-center text-black bg-transparent border-none outline-none cursor-pointer">
+                        {{ $item['label'] }}
+                    </button>
+                @endif
+                
             </li>
             @endforeach
         </ul>
