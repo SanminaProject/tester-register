aa00bcc540f2cae6ae12ef5c6ab20fbdffe373c7
Helmi Griffiths <t3grhe00@students.oamk.fi>
2026-04-14 22:33:45 +0300

    fix: fix issue with fixtures page not being loaded due to hardcoded filter values in data table component

--- app/Livewire/Components/DataTable.php
+++ app/Livewire/Components/DataTable.php
@@ -6,6 +6,7 @@ use Livewire\Component;
 use Livewire\WithPagination;
 use App\Models\Tester;
 use App\Models\Fixture;
+use Illuminate\Support\Str;
 
 class DataTable extends Component
 {
@@ -20,10 +21,13 @@ class DataTable extends Component
     public $searchPlaceholder = 'Search...';
     public $addButtonLabel = 'Add';
 
+    public $filters = [];
+    public $activeFilters = [];
+
     public function getHasDetailsProperty()
     {
-        $plural = \Illuminate\Support\Str::plural($this->type);
-        $singular = \Illuminate\Support\Str::singular($this->type);
+        $plural = Str::plural($this->type);
+        $singular = Str::singular($this->type);
         return view()->exists("livewire.pages.{$plural}.{$singular}-details");
     }
 
@@ -36,6 +40,15 @@ class DataTable extends Component
         };
     }
 
+    protected function getRelations()
+    {
+        return match ($this->type) {
+            'testers' => ['owner', 'statusRelation'],
+            'fixtures' => [],
+            default => [],
+        };
+    }
+
     protected function getSearchColumns()
     {
         return match ($this->type) {
@@ -43,6 +56,38 @@ class DataTable extends Component
             'fixtures' => ['name', 'description', 'manufacturer'],
             default => [],
         };
+    }
+
+    protected function getFiltersConfig()
+    {
+        return match ($this->type) {
+            'testers' => [
+                'id' => 'ID',
+                'name' => 'Name',
+                'description' => 'Description',
+                'operating_system' => 'OS',
+                'owner_id' => 'Owner',
+                'status_id' => 'Status',
+            ],
+            'fixtures' => [
+                'id' => 'ID',
+                'name' => 'Name',
+                'description' => 'Description',
+                'manufacturer' => 'Manufacturer',
+            ],
+            default => [],
+        };
+    }
+
+    public function toggleFilter($filter)
+    {
+        if (in_array($filter, $this->activeFilters)) {
+            $this->activeFilters = array_values(array_diff($this->activeFilters, [$filter]));
+        } else {
+            $this->activeFilters[] = $filter;
+        }
+
+        $this->resetPage();
     }
 
     public function updatingSearch(): void
@@ -53,13 +98,8 @@ class DataTable extends Component
     public function render()
     {
         $model = $this->getModelClass();
-
-
-        $query = $model::with([
-            'owner',
-            'statusRelation'
-        ]);
-
+        $this->filters = $this->getFiltersConfig();
+        $query = $model::with($this->getRelations());
         $keyword = trim($this->search);
 
         if ($keyword !== '') {
