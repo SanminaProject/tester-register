08d19839e89b129a351cbac182ad54da1f274772
Helmi Griffiths <t3grhe00@students.oamk.fi>
2026-04-26 22:40:59 +0300

    feat: add feature tests for admin page

---
+++ app/Livewire/Pages/Admin/Personnel/PersonnelTable.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Pages\Admin\Personnel;
+
+use Livewire\Component;
+
+class PersonnelTable extends Component
+{
+    public function render()
+    {
+        return view('livewire.pages.admin.personnel.personnel-table');
+    }
+}
